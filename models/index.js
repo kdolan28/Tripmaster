e48ea91a4dec0f79b840fa29dae51e165b688112
Vaughan Knouse <vaughanknouse@gmail.com>
2024-07-20 15:04:31 -0400

    edited index.js in model file due to an error

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-/*import User from './User.js';
-import Comment from './Comment.js';
+import User from './User.js';
+//import Comment from './Comment.js';
 
-// Creates a relationship between User and Comment model, with the User having a "has many" relationship with Comment model.
+/*// Creates a relationship between User and Comment model, with the User having a "has many" relationship with Comment model.
 User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
@@ -10,6 +10,7 @@ User.hasMany(Comment, {
 // Creates a relationship between User and Comment model, with a "belongs to" relationship of the Comment to the User.
 Comment.belongsTo(User, {
   foreignKey: 'user_id'
-});
+});*/
 
-export { User, Comment };*/
+//export { User, Comment };
+export { User };
