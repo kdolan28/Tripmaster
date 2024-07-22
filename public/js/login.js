d00a5fd65b1b9d3780728a10e7b99ab8b6c7b7dd
Vaughan Knouse <vaughanknouse@gmail.com>
2024-07-22 13:12:38 -0400

    edited login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,27 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-    if (email && password) {
-      // Send a POST request to the API endpoint
+  if (email && password) {
+    // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/');
     } else {
-        alert('Please enter email and password');
+      alert('Please enter email and password');
     }
-    }
+  }
 };
 
-
 document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
